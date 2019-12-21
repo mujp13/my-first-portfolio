8e7ccb1eeabe99f3fefe28059900c467c99f84ca
Kevin Park <keunsuk413@gmail.com>
2019-12-21 02:08:15 -0600

    navbar improvement

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
-let mainNav = document.getElementById("js-menu");
-let navBarToggle = document.getElementById("js-navbar-toggle");
+let navBarToggle = document.getElementById('js-navbar-toggle');
+let mainNav = document.getElementById('js-menu');
 
-navBarToggle.addEventListener("click", function() {
-  mainNav.classList.toggle("active");
+navBarToggle.addEventListener('click', function() {
+  mainNav.classList.toggle('active');
 });
 
+function handleMenuItemClick() {
+  $('.nav-links').on('click', function(event) {
+    $('.main-nav').hide();
+  });
+}
 
+function handleMenuIconClick(){
+  //listen for click on burger icon
+  // show main nav
+  $('.navbar-toggle').on('click', function(event){
+      $('.main-nav').show();
+  })
+}
+
+function callFns() {
+  handleMenuItemClick();
+  handleMenuIconClick();
+}
+
+$(callFns());
